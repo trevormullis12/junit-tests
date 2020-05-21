05ce7a3da31d78e72cbcd460fb8ee9ce0105a1cf
Fer <fernandoza@gmail.com>
2020-05-21 16:48:34 -0500

    feat: finish the exercise

--- src/test/java/CohortTest.java
+++ src/test/java/CohortTest.java
@@ -7,39 +7,42 @@ import static org.junit.Assert.*;
 
 public class CohortTest {
 
-    Cohort emptyCohort;
-    Cohort cohortWithOne;
-    Cohort cohortWithMany;
-
-    @Before
-    public void setup(){
-        emptyCohort = new Cohort();
-        cohortWithOne = new Cohort();
-        cohortWithMany = new Cohort();
-
-        Student fer = new Student(1, "Fer M");
-        fer.addGrade(90);
-
-        Student trant = new Student(2, "Trant B");
-        trant.addGrade(100);
-        trant.addGrade(100);
-        trant.addGrade(100);
-
-        cohortWithOne.addStudent(fer);
-        cohortWithMany.addStudent(fer);
-        cohortWithMany.addStudent(trant);
+    @Test
+    public void testIfAddStudentWorks(){
+        Cohort apex = new Cohort();
+        Student fer = new Student(1L, "Fer");
+        Student douglas = new Student(3L, "douglas");
+        apex.addStudent(fer);
+        apex.addStudent(douglas);
+        assertEquals(1L, apex.getStudents().get(0).getId());
+        assertEquals(3L, apex.getStudents().get(1).getId());
+        assertEquals("Fer", apex.getStudents().get(0).getName());
     }
 
     @Test
-    public void testAddStudentAndGetStudentsWork(){
-        assertEquals(0, emptyCohort.getStudents().size());
-        assertEquals(1, cohortWithOne.getStudents().size());
-//        assertEquals(1, cohortWithOne.getStudents().get(0).getId());
+    public void testIfGetStudentsWorks(){
+        Cohort apex = new Cohort();
+        assertEquals(0, apex.getStudents().size());
+        Student fer = new Student(1L, "Fer");
+        apex.addStudent(fer);
+        assertEquals(1, apex.getStudents().size());
     }
 
     @Test
-    public void testIfAvgIsCorrect(){
-        assertEquals(95.0, cohortWithMany.getCohortAverage(), 0);
+    public void testIfAvgWorks(){
+        Cohort apex = new Cohort();
+        Student fer = new Student(1L, "Fer");
+        fer.addGrade(100);
+        fer.addGrade(100);
+        // 100 + 100 / 2 = 100
+        Student douglas = new Student(3L, "douglas");
+        douglas.addGrade(80);
+        douglas.addGrade(100);
+        // 80 + 100 / 2 = 90
+        apex.addStudent(fer);
+        apex.addStudent(douglas);
+        // 190 / 2 == 95
+        assertEquals(95, apex.getCohortAverage(), 0);
     }
 
 }
